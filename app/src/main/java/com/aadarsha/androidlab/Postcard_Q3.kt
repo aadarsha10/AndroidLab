b55de23c4df3453081d26ddf8e7de865ac6511d0
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2020-12-08 12:36:02 +0545

    Question 3

---
+++ app/src/main/java/com/aadarsha/androidlab/Postcard_Q3.kt
@@ -0,0 +1,11 @@
+package com.aadarsha.androidlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Postcard_Q3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postcard__q3)
+    }
+}
